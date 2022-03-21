0b2ff9c091fc0d8caa865d06bf535e1b1030d5e8
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-21 11:27:59 +0400

    create next.config.js with IP whitelist for Images

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.export = {
+    images:{
+        domains:['links.papareact.com', 'fakestoreapi.com']
+    }
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
-
+import Image from "next/image";
 function Header() {
   return (
     <header>
-        {/* TOP NAV */}
-        <div>
+      {/* TOP NAV */}
+      <div>
+        <Image
+        src="https://links.papareact.com/f90"
+        width={150}
+        height={40} />
+      </div>
 
-        </div>
-
-        {/* BOTTOM NAV */}
-        <div>
-            
-        </div>
+      {/* BOTTOM NAV */}
+      <div></div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
