aea88c791417c5d61a00752fc14cfa29dfc7c4bf
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-21 14:58:48 +0400

    build the right section of the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,16 +33,17 @@ function Header() {
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
           <div className="link">
             <p>Hello Ali M.</p>
-            <p>Account & Lists</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
 
           <div className="link">
             <p>Returns</p>
-            <p>& Orders</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="link">
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 w-4 h-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
             <ShoppingCartIcon className="h-10"/>
-            <p>Basket</p>
+            <p className="hidden md:inline font-extrabold mt-2 md:text-sm">Basket</p>
           </div>
         </div>
       </div>
