77f976773e2db30b33391607bb4a038f8e0b3963
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-21 18:16:31 +0400

    build the Banner Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100 h-screen">
       <Head>
         <title>Amazon Clone</title>
       </Head>
@@ -13,7 +14,7 @@ export default function Home() {
     <main>
       {/* Banner */}
     <div className='max-w-screen-2xl mx-auto'>
-
+    <Banner />
     </div>
       {/* ProductFeed */}
     </main>
