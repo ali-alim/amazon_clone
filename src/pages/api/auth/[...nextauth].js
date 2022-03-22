572f367bbde5617f147d7979c55d0d0083df6dfc
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-22 14:17:29 +0400

    feat: authorizing redirect URI's

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
     providers: [
-        Providers.Google({
+        GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
