79420dc7426bff9092112c02fe02c8f888f46db8
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-22 12:51:27 +0400

    create firebase.js and add firebaseConfig

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8dqVhsRXuxdU72ZuNbltoLo3Hqarp1aI",
+    authDomain: "ali-86ebe.firebaseapp.com",
+    projectId: "ali-86ebe",
+    storageBucket: "ali-86ebe.appspot.com",
+    messagingSenderId: "366288037576",
+    appId: "1:366288037576:web:7ed607ae72a25833b527fd"
+  };
