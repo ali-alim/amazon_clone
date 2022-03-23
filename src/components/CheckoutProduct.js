c2580509e8439c11c0294a3276d94abfaa01d47c
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-23 22:18:51 +0400

    add title, description and rating

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,25 +1,34 @@
-import Image from "next/image"
+import { StarIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 
 function CheckoutProduct({
-    id,
-    title,
-    price,
-    rating,
-    description,
-    category,
-    image,
-    hasPrime
+  id,
+  title,
+  price,
+  rating,
+  description,
+  category,
+  image,
+  hasPrime,
 }) {
   return (
     <div className="grid grid-cols-5">
-        <Image
-            src={image}
-            width={200}
-            height={200}
-            objectFit="contain"
-        />
+      <Image src={image} width={200} height={200} objectFit="contain" />
+      <div className="col-span-3 mx-5">
+        <p>{title}</p>
+        <div className="flex">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon key={id} className="h-5 text-yellow-500" />
+            ))}
+        </div>
+        <p className="text-xs my-2 line-clamp-3">
+          {description}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CheckoutProduct
+export default CheckoutProduct;
