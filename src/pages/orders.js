b35f79a6c4e73f3a5e869bfea6236ed22813852b
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-25 23:18:24 +0400

    build the Order component

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -2,8 +2,10 @@ import moment from "moment";
 import { getSession, useSession } from "next-auth/react";
 import db from "../../firebase";
 import Header from "../components/Header";
+import Order from "../components/Order";
 function Orders({orders}) {
   const { data: session, status } = useSession();
+  console.log(orders)
   return (
     <div>
       <Header />
@@ -12,14 +14,23 @@ function Orders({orders}) {
           Your order
         </h1>
         {session ? (
-          <h2>x Orders</h2>
+          <h2>{orders.length} Orders</h2>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
 
         <div className="mt-5 space-y-4">
-          {orders.map((order) => (
-            <Order />
+          {orders?.map(
+              ({id, amount, amountShipping, items, timestamp, images}) => (
+            <Order  
+                key={id}
+                id={id}
+                amount={amount}
+                amountShipping={amountShipping}
+                items={items}
+                timestamp={timestamp}
+                images={images}
+            />
           ))}
         </div>
       </main>
