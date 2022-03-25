81cda1ac0c831d5d6d748bc2a42a37dbd86acce4
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-25 18:24:42 +0400

    started building the Orders page

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8dqVhsRXuxdU72ZuNbltoLo3Hqarp1aI",
-    authDomain: "ali-86ebe.firebaseapp.com",
-    projectId: "ali-86ebe",
-    storageBucket: "ali-86ebe.appspot.com",
-    messagingSenderId: "366288037576",
-    appId: "1:366288037576:web:7ed607ae72a25833b527fd"
-  };
+  apiKey: "AIzaSyD8dqVhsRXuxdU72ZuNbltoLo3Hqarp1aI",
+  authDomain: "ali-86ebe.firebaseapp.com",
+  projectId: "ali-86ebe",
+  storageBucket: "ali-86ebe.appspot.com",
+  messagingSenderId: "366288037576",
+  appId: "1:366288037576:web:7ed607ae72a25833b527fd",
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+  const db = app.firestore();
+
+  export default db; 
